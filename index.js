8abf985728e35f3d25b8382d6a7f2070144088c1
Aleksandra <alexandra.bakhcheva@gmail.com>
2022-08-10 21:12:54 +0500

    js update

--- index.js
+++ index.js
@@ -13,44 +13,48 @@ form.addEventListener("submit", function(event) {
 });
 
 // ВЫВОД ОШИБКИ НА НЕЗАПОЛНЕННОЕ ПОЛЕ
-function errorMessage(input, msg) {
+function setErrorMessage(input, msg) {
     document.querySelector(input).style.color = ERRORCOLOR;
     document.querySelector(msg).innerHTML = IMPORTANTMESSAGE;
 }
 
-// ПРОВЕРКА ЗАПОЛНЕННОСТИ ПОЛЕЙ И ПРИВЕТСТВИЕ
+// ПРОВЕРКА НА НЕЗАПОЛНЕННОЕ ПОЛЕ 
+function isFieldEmpty(field) {
+    let inputField = document.querySelector(field).value;
+    if (inputField === '') {
+        return true;
+    }
+}
+
+// ПРОВЕРКА ЗАПОЛНЕННОСТИ ВСЕХ ПОЛЕЙ И ПРИВЕТСТВИЕ
 function checkInput() {
-    let surname = document.querySelector('.input1').value;
-    if (surname == '') {
-        errorMessage('.surname', '.mandatory1');
+    if (isFieldEmpty(".input1")) {
+        setErrorMessage('.surname', '.mandatory1');
     }
-    let name = document.querySelector('.input2').value;
-    if (name == '') {
-        errorMessage('.name', '.mandatory2');
+    if (isFieldEmpty(".input2")) {
+        setErrorMessage('.name', '.mandatory2');
     }
-    let email = document.querySelector('.input3').value;
-    if (email == '') {
-        errorMessage('.email', '.mandatory3');
+    if (isFieldEmpty(".input3")) {
+        setErrorMessage('.email', '.mandatory3');
     }
-    let phone = document.querySelector('.input4').value;
-    if (phone == '') {
-        errorMessage('.phone', '.mandatory4');
+    if (isFieldEmpty(".input4")) {
+        setErrorMessage('.phone', '.mandatory4');
     }
-    let password1 = document.querySelector('.input5').value;
-    if (password1 == '') {
-        errorMessage('.password1', '.mandatory5');
+    if (isFieldEmpty(".input5")) {
+        setErrorMessage('.password1', '.mandatory5');
     }
-    let password2 = document.querySelector('.input6').value;
-    if (password2 == '') {
-        errorMessage('.password2', '.mandatory6');
+    if (isFieldEmpty(".input6")) {
+        setErrorMessage('.password2', '.mandatory6');
     }
-    if (surname !== '' && name !== '' && email !== '' && phone !== '' && password1 !== '' && password2 !== '') {
-        document.querySelector('.welcome').innerHTML = (`Добро пожаловать, ${name}!`);
+
+    let nameGreeting = document.querySelector(".input2").value;
+    if (!isFieldEmpty(".input1") && !isFieldEmpty(".input2") && !isFieldEmpty(".input3") && !isFieldEmpty(".input4") && !isFieldEmpty(".input5") && !isFieldEmpty(".input6")) {
+        document.querySelector('.welcome').innerHTML = (`Добро пожаловать, ${nameGreeting}!`);
     }
 }
 
 // ПРОВЕРКА НА ВВОД ОБЯЗАТЕЛЬНЫХ ПОЛЕЙ
-function InputProvided(input, clear) {
+function provideInput(input, clear) {
     document.querySelector(input).style.color = CORRECTCOLOR;
     document.querySelector(clear).innerHTML = "";
 }
@@ -59,61 +63,61 @@ const FIOCHECK = /^[а-яА-ЯЁё]{2,20}$/;
 
 let inputSurname = document.querySelector('.input1');
 inputSurname.addEventListener("keyup", function() {
-    InputProvided('.surname', '.mandatory1');
-    inputCheck(FIOCHECK, inputSurname, '.surname', '.mandatory1', INCORRECTLANG);
+    provideInput('.surname', '.mandatory1');
+    isInputValid(FIOCHECK, inputSurname, '.surname', '.mandatory1', INCORRECTLANG);
 });
 
 let inputName = document.querySelector('.input2');
 inputName.addEventListener("keyup", function() {
-    InputProvided('.name', '.mandatory2');
-    inputCheck(FIOCHECK, inputName, '.name', '.mandatory2', INCORRECTLANG);
+    provideInput('.name', '.mandatory2');
+    isInputValid(FIOCHECK, inputName, '.name', '.mandatory2', INCORRECTLANG);
 });
 
 let inputEmail = document.querySelector('.input3');
 inputEmail.addEventListener("keyup", function() {
-    InputProvided('.email', '.mandatory3');
+    provideInput('.email', '.mandatory3');
     const EMAILCHECK = /^([\w\d._\-#])+@([\w\d._\-#]+[.][\w\d._\-#]+)$/i;
-    inputCheck(EMAILCHECK, inputEmail, '.email', '.mandatory3', INCORRECTLANG);
+    isInputValid(EMAILCHECK, inputEmail, '.email', '.mandatory3', INCORRECTLANG);
 });
 
 let inputPhone = document.querySelector('.input4');
 inputPhone.addEventListener("keyup", function() {
-    InputProvided('.phone', '.mandatory4');
+    provideInput('.phone', '.mandatory4');
     const PHONECHECK = /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/;
-    inputCheck(PHONECHECK, inputPhone, '.phone', '.mandatory4', PHONEMESSAGE);
+    isInputValid(PHONECHECK, inputPhone, '.phone', '.mandatory4', PHONEMESSAGE);
 });
 
 const PASSWORDCHECK = /^(?=.*\d)(?=.*[a-z])(?=.*[!@#$&*])(?=.*[A-Z])(?=(.*[a-zA-Z]){4}).{8,20}$/;
 
 let inputPassword1 = document.querySelector('.input5');
 inputPassword1.addEventListener("keyup", function() {
-    InputProvided('.password1', '.mandatory5');
-    inputCheck(PASSWORDCHECK, inputPassword1, '.password1', '.mandatory5', INCORRECTPASSWORD);
+    provideInput('.password1', '.mandatory5');
+    isInputValid(PASSWORDCHECK, inputPassword1, '.password1', '.mandatory5', INCORRECTPASSWORD);
 });
 
 let inputPassword2 = document.querySelector('.input6');
 inputPassword2.addEventListener("keyup", function() {
-    InputProvided('.password2', '.mandatory6');
+    provideInput('.password2', '.mandatory6');
 });
 
-inputPassword2.addEventListener("keyup", passwordValidation);
+inputPassword2.addEventListener("keyup", isPasswordValid);
 
 const QUESTION_REPLYCHECK = /^[?!,.а-яА-ЯёЁ0-9\s]+$/;
 
 let inputQuestion = document.querySelector('.question');
 inputQuestion.addEventListener("keyup", function() {
-    InputProvided('.control-question', '.mandatory7');
-    inputCheck(QUESTION_REPLYCHECK, inputQuestion, '.control-question', '.mandatory7', QUESTION_REPLY);
+    provideInput('.control-question', '.mandatory7');
+    isInputValid(QUESTION_REPLYCHECK, inputQuestion, '.control-question', '.mandatory7', QUESTION_REPLY);
 });
 
 let inputReply = document.querySelector('.reply');
 inputReply.addEventListener("keyup", function() {
-    InputProvided('.control-reply', '.mandatory8');
-    inputCheck(QUESTION_REPLYCHECK, inputReply, '.control-reply', '.mandatory8', QUESTION_REPLY);
+    provideInput('.control-reply', '.mandatory8');
+    isInputValid(QUESTION_REPLYCHECK, inputReply, '.control-reply', '.mandatory8', QUESTION_REPLY);
 });
 
 // ПРОВЕРКА НА СОВПАДЕНИЕ ПАРОЛЕЙ 
-function passwordValidation() {
+function isPasswordValid() {
     if (inputPassword1.value !== inputPassword2.value) {
         document.querySelector('.mandatory6').innerHTML = "Пароль не совпадает"; 
     }
@@ -123,7 +127,7 @@ function passwordValidation() {
 }   
 
 // ВАЛИДАЦИЯ ПОЛЯ 
-function inputCheck(regex, input, labelcolor, message, error) {
+function isInputValid(regex, input, labelcolor, message, error) {
     if (regex.test(input.value) == false) {
         document.querySelector(labelcolor).style.color = ERRORCOLOR;
         document.querySelector(message).innerHTML = error;
